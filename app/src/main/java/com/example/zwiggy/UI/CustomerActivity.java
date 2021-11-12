37ce6a39c11ad1068d17721a1d12ebdfb94b0844
This PC <bansal.yash.130@gmail.com>
2021-11-12 20:52:12 +0530

    new changes

---
+++ app/src/main/java/com/example/zwiggy/UI/CustomerActivity.java
@@ -0,0 +1,15 @@
+package com.example.zwiggy.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.zwiggy.R;
+
+public class CustomerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer);
+    }
+}
