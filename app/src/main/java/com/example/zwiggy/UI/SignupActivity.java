0d834744f1727a37a0104be7895fa4da779a28d1
Utkarsh <utkarshdubey0987@gmail.com>
2021-11-12 21:39:11 +0530

    3rd today

--- app/src/main/java/com/example/zwiggy/UI/SignupActivity.java
+++ app/src/main/java/com/example/zwiggy/UI/SignupActivity.java
@@ -3,14 +3,31 @@ package com.example.zwiggy.UI;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.zwiggy.R;
 
 public class SignupActivity extends AppCompatActivity {
+    EditText editSignUpName;
+    EditText editSignUpEmail;
+    EditText editSignUpPassword;
+    Button signUpButton;
+    TextView signUpToLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        editSignUpEmail = findViewById(R.id.inputSignUpName);
+        editSignUpEmail = findViewById(R.id.inputSignUpEmail);
+        editSignUpPassword = findViewById(R.id.inputSignUpPassword);
+        signUpButton = findViewById(R.id.signUpButton);
+        signUpToLogin = findViewById(R.id.signUpToLogin);
+
+//        signUpToLogin.setOnClickListener(signUpToLoginClick);
+//        signUpButton.setOnClickListener(signUpButtonClick);
     }
 }
