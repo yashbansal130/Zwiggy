4a8b5bf9ce95852680fe5f6b67abb334c4822d64
This PC <bansal.yash.130@gmail.com>
2021-11-12 21:42:52 +0530

    many more changes

--- app/src/main/java/com/example/zwiggy/UI/MainActivity.java
+++ app/src/main/java/com/example/zwiggy/UI/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.zwiggy.Data.UserDetail;
 import com.example.zwiggy.R;
 
 import java.util.concurrent.atomic.AtomicReference;
@@ -23,71 +24,38 @@ import io.realm.mongodb.User;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button mainCustomer;
+    Button mainOwner;
 
-    EditText editLoginEmail;
-    EditText editLoginPassword;
-    Button loginButton;
-    TextView loginSignup;
-    App app;
-    String appID = "hackit-qyzey";
-    String email = "test@gmail.com";
-    String password = "password";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Realm.init(this);
-
-        editLoginEmail = findViewById(R.id.inputLoginEmail);
-        editLoginPassword = findViewById(R.id.inputLoginPassword);
-        loginButton = findViewById(R.id.loginButton);
-        loginSignup = findViewById(R.id.loginSignup);
-
-        loginSignup.setOnClickListener(LoginSignupClick);
-        loginButton.setOnClickListener(LoginButtonClick);
-
-        app = new App(new AppConfiguration.Builder(appID)
-                .build());
-//        app.getEmailPassword().confirmUserAsync(email, password, it -> {
-//            if (it.isSuccess()) {
-//                Log.i("EXAMPLE", "Successfully registered user.");
-//            } else {
-//                Log.e("EXAMPLE", "Failed to register user: " + it.getError().getErrorMessage());
-//            }
-//        });
+        mainCustomer= findViewById(R.id.mainCustomer);
+        mainOwner = findViewById(R.id.mainOwner);
+        mainOwner.setOnClickListener(mainOwnerClick);
+        mainCustomer.setOnClickListener(mainCustomerClick);
     }
-    View.OnClickListener LoginSignupClick = new View.OnClickListener() {
+    View.OnClickListener mainOwnerClick = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-
-            Intent intent = new Intent(MainActivity.this, SignupActivity.class);
-            startActivity(intent);
+            UserDetail.setType(1);
+            getToLogin();
 
         }
     };
 
-    View.OnClickListener LoginButtonClick = new View.OnClickListener() {
+    View.OnClickListener mainCustomerClick = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            Credentials emailPasswordCredentials = Credentials.emailPassword(email, password);
-            app.loginAsync(emailPasswordCredentials, it -> {
-                if (it.isSuccess()) {
-                    Log.i("EXAMPLE", "Successfully registered user.");
-                    Toast.makeText(MainActivity.this,"Login Successful", Toast.LENGTH_SHORT).show();
-                    if(this == this/* will fill */){
-                        Intent intent = new Intent(MainActivity.this, CustomerActivity.class);
-                        startActivity(intent);
-                    }else{
-                        Intent intent = new Intent(MainActivity.this, SignupActivity.class);
-                        startActivity(intent);
-                    }
-                } else {
-                    Log.e("EXAMPLE", "Failed to register user: " + it.getError().getErrorMessage());
-                    Toast.makeText(MainActivity.this,"Login Failed, Try Again", Toast.LENGTH_SHORT).show();
-                }
-            });
+            UserDetail.setType(0);
+            getToLogin();
         }
     };
 
+    private void getToLogin(){
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+    }
 
 }
--- app/src/main/java/com/example/zwiggy/UI/SignupActivity.java
+++ app/src/main/java/com/example/zwiggy/UI/SignupActivity.java
@@ -8,9 +8,18 @@ import com.example.zwiggy.R;
 
 public class SignupActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+//        app.getEmailPassword().confirmUserAsync(email, password, it -> {
+//            if (it.isSuccess()) {
+//                Log.i("EXAMPLE", "Successfully registered user.");
+//            } else {
+//                Log.e("EXAMPLE", "Failed to register user: " + it.getError().getErrorMessage());
+//            }
+//        });
     }
 }
