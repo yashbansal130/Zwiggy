da33564bc69dac7af061b0bfb69e20335827e6a0
yogi230 <yogesh.dhanuka.che19@itbhu.ac.in>
2021-11-12 22:35:37 +0530

    my changes

--- app/src/main/java/com/example/zwiggy/MainActivity.java
+++ app/src/main/java/com/example/zwiggy/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -14,32 +16,34 @@ import io.realm.mongodb.Credentials;
 import io.realm.mongodb.User;
 
 public class MainActivity extends AppCompatActivity {
-
+    String appID = "hackit-qyzey";
+    String email = "test@gmail.com";
+    String password = "password";
+    Button loginBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Realm.init(this);
 
-        String appID = "hackit-qyzey"; // replace this with your App ID
+        Realm.init(this);
         App app = new App(new AppConfiguration.Builder(appID)
                 .build());
-        String email = "test@gmail.com";
-        String password = "password";
-//        app.getEmailPassword().confirmUserAsync(email, password, it -> {
-//            if (it.isSuccess()) {
-//                Log.i("EXAMPLE", "Successfully registered user.");
-//            } else {
-//                Log.e("EXAMPLE", "Failed to register user: " + it.getError().getErrorMessage());
-//            }
-//        });
-        Credentials emailPasswordCredentials = Credentials.emailPassword(email, password);
-        app.loginAsync(emailPasswordCredentials, it -> {
-            if (it.isSuccess()) {
-                Log.i("EXAMPLE", "Successfully registered user.");
-            } else {
-                Log.e("EXAMPLE", "Failed to register user: " + it.getError().getErrorMessage());
+        loginBtn=(Button)findViewById(R.id.loginButton);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Credentials emailPasswordCredentials = Credentials.emailPassword(email, password);
+                app.loginAsync(emailPasswordCredentials, it -> {
+                    if (it.isSuccess()) {
+                        Log.i("EXAMPLE", "Successfully registered user.");
+
+                    } else {
+                        Log.e("EXAMPLE", "Failed to register user: " + it.getError().getErrorMessage());
+                    }
+                });
             }
         });
+
     }
+
 }
